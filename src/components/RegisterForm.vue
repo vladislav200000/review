ceb67dccdabf0a2c8755ab401966239c0b408afd
Ryabinin Vladislav <gallooff@yandex.ru>
2024-10-02 17:33:32 +0500

    Переход со страницы регистрации на логин и тд.

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -50,6 +50,7 @@
       <button
         type="submit"
         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        href="/login"
       >
         Зарегистрироваться
       </button>
@@ -58,6 +59,7 @@
 </template>
 <script>
 import axios from 'axios'
+import router from '@/router'
 
 export default {
   data() {
@@ -76,6 +78,7 @@ export default {
           password: this.password
         })
         alert('User registered successfully')
+        router.push({ path: '../login' })
       } catch (error) {
         console.error(error)
       }
